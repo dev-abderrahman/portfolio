909735ba23ca3766ecf12c62a4395a5c0d28ac71
dev-elmoak <developer14elmouak@gmail.com>
2025-08-04 00:31:00 +0200

    portfolio

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const loading = document.querySelector(".loading");
+  const portfolioWrapper = document.querySelector(".portfolio-wrapper");
+  loading.style.display = "none";
+  portfolioWrapper.style.display = "block";
+});
