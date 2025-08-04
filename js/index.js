39ed16ee27b193070f892bc57ae4d42dc9135147
dev-elmoak <developer14elmouak@gmail.com>
2025-08-04 02:13:57 +0200

    change stricture

--- js/index.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-  const loading = document.querySelector(".loading");
-  const portfolioWrapper = document.querySelector(".portfolio-wrapper");
-  loading.style.display = "none";
-  portfolioWrapper.style.display = "block";
-});
